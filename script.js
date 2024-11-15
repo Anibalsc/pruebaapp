5338f483c8062a474457f54d411d1e157f31b716
Anibalsc <anibal.santanac@gmail.com>
2024-11-15 17:16:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -17,23 +17,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Probar guardado manual de datos
-function testFirebaseWrite() {
-    const testRef = db.ref('test');
-    testRef.set({
-        message: "Hola Firebase",
-        timestamp: Date.now()
-    })
-    .then(() => {
-        console.log("Datos guardados correctamente en Firebase.");
-        alert("Guardado exitoso: 'Hola Firebase'");
-    })
-    .catch((error) => {
-        console.error("Error al guardar datos en Firebase:", error);
-        alert("Error al guardar datos. Revisa la consola para más detalles.");
-    });
-}
-
 let map;
 let products = [];
 
